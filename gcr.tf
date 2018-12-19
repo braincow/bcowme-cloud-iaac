32f88c3ccf30ee794a76b47a8ea6a4d9f62bc03f
Antti Peltonen <antti.peltonen@solita.fi>
2018-12-19 11:32:33 +0200

    GKE required oauth context for GCR access. GCR reference added.

---
+++ gcr.tf
@@ -0,0 +1,4 @@
+# just to document that we have one, we create a data reference to GCR in our project
+data "google_container_registry_repository" "bcows-lab-gcr" {}
+
+# eof
--- gkecluster.tf
+++ gkecluster.tf
@@ -2,7 +2,7 @@
 resource "google_container_cluster" "gke-cluster-3" {
   name = "gke-cluster-3"
   network = "default"
-  zone = "europe-north1-b"
+  zone = "europe-north1-a"
   initial_node_count = 1
 
   # gke version, both need to exist and be the same
@@ -10,6 +10,14 @@ resource "google_container_cluster" "gke-cluster-3" {
   #min_master_version = "1.11.3-gke.18"
   node_config {
     machine_type = "g1-small"
+
+    # some oauth scopes are required after all
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
   }
 }
 # eof
